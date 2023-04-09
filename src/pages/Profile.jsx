407915ff0086ad6c21950ecf0bfbaf824aaea6c3
Gargi Roy <itsgargiroy@gmail.com>
2023-04-10 00:55:10 +0530

    LogOut button added, Wrong credentials alert added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import { useState, useEffect } from "react";
 
 function Profile() {
+  const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const OnLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={OnLogout}>
+          {" "}
+          Logout
+        </button>
+      </header>
     </div>
   );
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 // ReactComponent not needed here as it'd be set as a source for an image tag.
@@ -18,6 +20,25 @@ function SignIn() {
       [e.target.id]: e.target.value,
     }));
   };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      // signInWithEmailAndPassword returns a promise, we'll put that promise ino userCredential
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("Bad User Credentials");
+    }
+  };
+
   return (
     <>
       <div className="pageContainer">
@@ -25,7 +46,7 @@ function SignIn() {
           <p className="pageHeader">Welcome Back!!</p>
         </header>
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input
               type="email"
               className="emailInput"
